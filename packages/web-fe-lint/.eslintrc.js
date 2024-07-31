95371b26880cdb6469ec3c4f841f9488b9bee2c8
dancingOfcode <zhengyou007@163.com>
2024-08-01 00:25:06 +0800

    feat: web-fe-lint cli脚手架测试

---
+++ packages/web-fe-lint/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['web-fe-eslint/typescript/node', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-require-imports': 0,
+    'no-console': 0,
+  },
+};
---
+++ packages/web-fe-lint/src/lints/eslint/getESLintConfigType.ts
@@ -0,0 +1,33 @@
+import glob from 'glob';
+import type { PKG } from '../../types';
+
+/**
+ * 获取 ESLint 配置类型
+ * @param cwd
+ * @param pkg
+ * @returns web-fe-eslint/index
+ * @returns web-fe-eslint/react
+ * @returns web-fe-eslint/typescript/index
+ * @returns web-fe-eslint/typescript/react
+ */
+export function getESLintConfigType(cwd: string, pkg: PKG): string {
+  const tsFiles = glob.sync('./!(node_modules)/**/*.@(ts|tsx)', { cwd });
+  const reactFiles = glob.sync('./!(node_modules)/**/*.@(jsx|tsx)', { cwd });
+  const vueFiles = glob.sync('./!(node_modules)/**/*.vue', { cwd });
+  const dependencies = Object.keys(pkg.dependencies || {});
+  const language = tsFiles.length > 0 ? 'typescript' : '';
+  let dsl = '';
+
+  // dsl判断
+  if (reactFiles.length > 0 || dependencies.some((name) => /^react(-|$)/.test(name))) {
+    dsl = 'react';
+  } else if (vueFiles.length > 0 || dependencies.some((name) => /^vue(-|$)/.test(name))) {
+    dsl = 'vue';
+  } else if (dependencies.some((name) => /^rax(-|$)/.test(name))) {
+    dsl = 'rax';
+  }
+
+  return (
+    'web-fe-eslint/' + `${language}/${dsl}`.replace(/\/$/, '/index').replace(/^\//, '')
+  );
+}
